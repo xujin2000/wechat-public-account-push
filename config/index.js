8d34410c8d0c6bb60a3540fc52fa191e118d101f
xujin2000 <114062461+xujin2000@users.noreply.github.com>
2022-09-22 10:36:35 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -64,7 +64,7 @@ export const config = {
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: "oTnFg6W4uGTISRyxi53BxR3fpnfc",
       // 你想对他发送的模板消息的模板ID
-      useTemplateId: "diF1EsqZDMDrvTxMkxU1_oN7xurfWUn5hFowjAcPTqg",
+      useTemplateId: "9x-V3m9_Xm8aropN-8P_-E6uaT89E1NJPDDwyUbEL0o",
       // 所在省份
       province: "湖南",
       // 所在城市
